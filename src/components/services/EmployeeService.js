668df3d7399c8cf1ca42e83a791003b6f67b9925
dwaynediddy <burrows.dwayne@icloud.com>
2022-04-13 16:48:58 +0200

    employee state changing locally

--- src/components/services/EmployeeService.js
+++ src/components/services/EmployeeService.js
@@ -4,29 +4,36 @@ export class EmployeeService {
             id: '1',
             name: 'Liv Evelyn Mosby',
             username: 'boyle',
-            email: 'yigetdomed@gmail.com'
+            email: 'yigetdomed@gmail.com',
+            isSelected: false
         },
         {
             id: '2',
             name: 'Hank Hill',
             username: 'Hank',
-            email: 'Propaine@gmail.com'
+            email: 'Propaine@gmail.com',
+            isSelected: false
         },
         {
             id: '3',
             name: 'Homer Simpson',
             username: 'Hommy',
-            email: 'mmmdonuts@hotmail.com'
+            email: 'mmmdonuts@hotmail.com',
+            isSelected: false
         },
         {
             id: '4',
             name: 'Charles Boyle',
-            username: 'bestfriendnumber2@gmail.com'
+            username: 'the duece',
+            email: 'bestfriendnumber2@gmail.com',
+            isSelected: false
         },
         {
             id: '5',
             name: 'Michael Scott',
-            username: 'michaelscarne@dundermifflen.com'
+            username: '#1Boss',
+            email: 'michaelscarne@dundermifflen.com',
+            isSelected: false
         },
     ]
 
